ac5261af9a25a4ffb8ef7641d3a3bddfe62af420
Yura <yuravigovskij@gmail.com>
2023-04-05 14:07:10 +0300

    finished DZ-21 (AJAX)

--- js/index.js
+++ js/index.js
@@ -1,110 +1,82 @@
-class Student {
-    constructor(firstName, lastName, birthday) {
-        this.firstName = firstName.trim();
-        this.lastName = lastName.trim();
-        this.birthday = new Date(birthday);
-        this.scores = [];
-        this.progress = new Array(25).fill(null);
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=DNIPRO&units=metric&APPID=5d066958a60d315387d9492393935c19';
+
+const response = fetch(url);
+response.then(data => {
+    return data.json();
+})
+    .then(data => {
+
+        document.querySelector('.js--form').addEventListener('input', (event) => {
+
+            if (event.target.value != 0) {
+                const searchParams = new URLSearchParams(url.split('?')[1]);
+                searchParams.set('q', event.target.value);
+                const newUrl = `${url.split('?')[0]}?${searchParams.toString()}`;
+
+                const newResponse = fetch(newUrl);
+                newResponse.then(data => {
+                    return data.json();
+                }).then(data => {
+                    const city = data.name;
+                    const description = data.weather[0].description;
+                    const iconId = data.weather[0].icon;
+
+                    const options = {
+                        temp: data.main.temp,
+                        tempMax: data.main.temp_max,
+                        tempMin: data.main.temp_min,
+                        pressure: data.main.pressure,
+                        speed: data.wind.speed,
+                        deg: data.wind.deg
+                    }
 
 
-        this.fullName = `${this.firstName} ${this.lastName}`;
-    }
-
-
-    getAge() {
-        return new Date().getFullYear() - this.birthday.getFullYear();
-    }
-
-    getAverageScore() {
-        if (this.scores.length === 0) return 'Scores are null';
-
-        const averageScore = this.scores.reduce((a, b) => (a + b)) / this.scores.length;
-        return averageScore.toFixed(1);
-    }
-
-    getAverageProgress() {
-        const averageProgress = this.progress.reduce((a, b) => (a + b)) / this.progress.length;
-        return averageProgress.toFixed(1);
-    }
-
-    present() {
-        this.setDay(true);
-    }
-
-    absent() {
-        this.setDay(false);
-    }
-
-
-    summary() {
-        console.log();
-        const averageScore = this.getAverageScore();
-        const averageProgress = this.getAverageProgress();
-        if (averageScore >= 90 && averageProgress >= 0.9) {
-            return 'Well done!!!'
-        } else if (averageScore < 90 && averageScore >= 70 || averageProgress < 0.9 && averageProgress >= 0.5) {
-            return 'Good but can better!';
-        } else {
-            return 'Radish!';
-        }
-    }
-
-
-    setDay(presentOrAbsent) {
-        if (this.progress[this.progress.length - 1] !== null || typeof presentOrAbsent !== 'boolean') return;
-
-        let index = 0;
-        for (let day of this.progress) {
-            if (day === null) {
-                this.progress[index] = presentOrAbsent;
-                return true;
+                    _city.innerHTML = city;
+                    _description.innerHTML = description;
+                    showIcon(_icon, iconId);
+                    _weatherBody.innerHTML = showOptions(options).innerHTML;
+                })
             }
+        })
 
-            index++;
+        const _weatherWrapper = document.querySelector('.js--weather');
+        const _city = _weatherWrapper.querySelector('.js--weather-city');
+        const _description = _weatherWrapper.querySelector('.js--weather-description');
+        const _weatherBody = _weatherWrapper.querySelector('.js--weather-body');
+        const _icon = _weatherWrapper.querySelector('.js--weather-icon');
+
+        const city = data.name;
+        const description = data.weather[0].description;
+        const iconId = data.weather[0].icon;
+
+        const options = {
+            temp: data.main.temp,
+            tempMax: data.main.temp_max,
+            tempMin: data.main.temp_min,
+            pressure: data.main.pressure,
+            speed: data.wind.speed,
+            deg: data.wind.deg
         }
-    }
-
-}
-
-// Test data
-const student1 = new Student('  Yura ', 'Vigovskyi  ', '05.02.2003');
-for (let index = 0; index < student1.progress.length; index++) {
-    const randNum = Math.round(Math.random() * 4);
-    if (randNum) {
-        student1.scores.push(100)
-        student1.present();
-    } else {
-        student1.absent();
-    }
-}
-console.log(student1.summary());
-// console.log(student1);
 
 
-const student2 = new Student('    Kolya', 'Ponchicov', '02.02.1999');
-for (let index = 0; index < student2.progress.length; index++) {
-    const randNum = Math.round(Math.random());
-    if (randNum) {
-        student2.scores.push(Math.round(Math.random() * 100))
-        student2.present();
-    } else {
-        student2.absent();
-    }
-}
-console.log(student2.summary());
-// console.log(student2);
+        _city.innerHTML = city;
+        _description.innerHTML = description;
+        showIcon(_icon, iconId);
+        _weatherBody.innerHTML = showOptions(options).innerHTML;
 
+        function showIcon(_icon, iconId) {
+            const urlIcon = 'https://openweathermap.org/img/w/'
+            _icon.setAttribute('src', urlIcon + iconId + '.png');
+        }
 
-const student3 = new Student('    Kolya', 'Ponchicov', '02.02.1999');
-for (let index = 0; index < student3.progress.length; index++) {
-    const randNum = Math.round(Math.random());
-    if (randNum) {
-        student3.scores.push(Math.round(Math.random() * 60))
-        student3.present();
-    } else {
-        student3.absent();
-    }
-}
-console.log(student3.summary());
-// console.log(student3);
+        function showOptions(options) {
+            const _optionsItem = document.createElement('div');
+            for (let optionsKey in options) {
+                const _newItem = document.createElement('div');
+                _newItem.innerHTML = `${optionsKey} : ${options[optionsKey]}`;
 
+                _optionsItem.append(_newItem);
+            }
+            return _optionsItem
+        }
+    });
