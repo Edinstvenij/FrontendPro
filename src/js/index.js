6c7445c98dcae360bce5511a6d35d60c2e6f52a9
Yura <yuravigovskij@gmail.com>
2023-04-22 13:05:36 +0300

    finished DZ-25 (async f, SWAPI) NO module

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,21 @@ const _form = document.querySelector('.js--form');
 
 _form.addEventListener('submit', function (event) {
     event.preventDefault();
+    const _spinnerTemplate = '<div class="spinner-border text-primary" role="status">\n' + '  <span class="visually-hidden">Loading...</span>\n' + '</div>';
+    const _button = event.submitter;
+    const buttonHtml = _button.innerHTML;
+    _button.innerHTML = _spinnerTemplate;
+    _button.disabled = true;
+
 
     const _input = _form.querySelector('#url');
     const urlParam = _input.value.trim();
 
     addStickers(urlParam);
-    addAnswer(requestToSWAPI(urlParam));
-
-
+    addAnswer(urlParam).finally(() => {
+        _button.innerHTML = buttonHtml;
+        _button.disabled = false;
+    });
 });
 
 function addStickers(urlParam) {
@@ -29,15 +36,28 @@ function addStickers(urlParam) {
     }
 }
 
-async function addAnswer(promise) {
+async function addAnswer(urlParam) {
     const _answer = document.querySelector('.js--answer');
+    const url = 'https://swapi.dev/api/' + urlParam;
+    const data = await fetch(url).then(promise => {
+        if (promise.status !== 200) {
+            throw error;
+        }
+        return promise.json();
+    }).catch(() => {
+        const _stickers = document.querySelectorAll('.sticker');
+
+        _stickers.forEach(_item => {
+            _item.classList.add('text-bg-danger');
+        });
+    });
 
     const _newPre = document.createElement('pre');
-    _newPre.textContent = JSON.stringify(await promise);
-    _answer.append(_newPre);
-}
+    _newPre.textContent = JSON.stringify(await data, null, 2);
 
-async function requestToSWAPI(urlParam) {
-    const url = 'https://swapi.dev/api/' + urlParam;
-    return await fetch(url).then(promise => promise.json());
+    while (_answer.firstChild) {
+        _answer.removeChild(_answer.firstChild);
+    }
+    _answer.append(_newPre);
+    return data;
 }
